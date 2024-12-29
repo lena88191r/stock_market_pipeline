f03085766af2cdb93ba40b0b91f76b861ffb8e0e
lena88191r <159266237+lena88191r@users.noreply.github.com>
2024-12-29 11:49:56 -0300

    Create database_connection.py

---
+++ database_connection.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+
+# Create a database connection
+
+engine = create_engine("postgresql://postgres:password@localhost:5432/stocks_db")  
+
+# Load data into the database
+
+data.to_sql("stocks", engine, if_exists="replace", index=False, method="multi")
+print("Data successfully loaded into the database!")
